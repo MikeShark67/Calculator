e1f56c796d015057ea2a6139d29c74a07312e894
MikeShark67 <oumam504@gmail.com>
2025-06-19 12:30:39 +0300

    added function

---
+++ indexj.js
@@ -0,0 +1,13 @@
+let Bill =4509.00;
+let people = 5;
+let percentage = 12;
+
+function calculateDiscount(Bill, percentage, people){
+    if (Bill!==4509.00) {
+      console.log("ijatosha");  
+    }else{
+        let perPersornCost = (Bill/ people) * (1 + percentage / 100);
+      console.log("unafaa :$"+ perPerperCost.toFixed(2));
+    }
+}
+calculateDiscount(Bill,percentage,people);
